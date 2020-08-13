da1c2fc479318ca3db8a8c131dcbaf95eea19792
Svyatoslav <khirigiri@gmail.com>
2020-08-13 21:12:18 +0700

    Edit main.cpp with r(f) and st(h)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include "incl.h"
+#include "st.h"
 extern void inst();
 extern string s(string &str);
+extern int r(vector<struct st> &rls, int N);
 
 int main() {
   inst();
   string str;
-  struct st {
-    string fp;
-    char es;
-    char ns;
-    char mv;
-    string np;
-  };
-  st rules[100];
+  vector<struct st> rls(100);
   s(str);
+  cout << "Ваша строка:   " << str;
+  int N = r(rls, 100);
+  cout << "Вы ввели правила:  " << endl;
+  for (int i = 0; i < N; i++)
+    cout << rls[i].fp << " " << rls[i].es << " " << rls[i].ns << " "
+         << rls[i].mv << " " << rls[i].np << endl;
   return 0;
 }
