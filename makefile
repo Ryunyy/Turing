a27594255e30a4f3a9a7b8aa6864a1e0ba2943d5
Maxim <eremenko98@yandex.ru>
2020-08-14 14:01:52 +0700

    Add move.cpp with functions

--- makefile
+++ makefile
@@ -6,7 +6,7 @@ s = src
 
 all: clean1 folders $(bi)/main
 
-$(bi)/main: $(bu)/main.o $(bu)/inst.o $(bu)/r.o $(bu)/s.o $(bu)/string_processing.o $(bu)/replacement_letters.o
+$(bi)/main: $(bu)/main.o $(bu)/inst.o $(bu)/r.o $(bu)/s.o $(bu)/string_processing.o $(bu)/replacement_letters.o $(bu)/move.o
 	$(flags) $^ -o $@
 
 $(bu)/main.o: $(s)/main.cpp $(s)/incl.h
@@ -25,6 +25,9 @@ $(bu)/string_processing.o: $(s)/string_processing.cpp $(s)/incl.h
 	$(flags) -c $< -o $@
 
 $(bu)/replacement_letters.o: $(s)/replacement_letters.cpp $(s)/incl.h
+	$(flags) -c $< -o $@
+
+$(bu)/move.o: $(s)/move.cpp $(s)/incl.h
 	$(flags) -c $< -o $@
 
 folders:
--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -1,6 +1,7 @@
 #include "incl.h"
 
 extern string replacement(vector<st> &rls, string &str, int i, int point);
+extern int move(int point, vector<st> &rls, int i);
 
 void string_processing(string &str, vector<st> &rls)
  {
@@ -9,9 +10,13 @@ void string_processing(string &str, vector<st> &rls)
     while(current_state != "halt"){
         if(current_state == rls[i].fp && str[point] == rls[i].es) {
             cout << replacement(rls, str, i, point) << endl;
+            point = move(point, rls, i);
+            current_state = rls[i].np;
+            i = 0;
         }
         else {
             i++;
         }
     }
+    return;
  }
