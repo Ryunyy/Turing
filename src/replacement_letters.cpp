6d1633f18287c1d090fab90273803b3d6e1bf310
Maxim <eremenko98@yandex.ru>
2020-08-14 14:07:47 +0700

    Added support for the expected symbol '*'

--- src/replacement_letters.cpp
+++ src/replacement_letters.cpp
@@ -1,6 +1,13 @@
 #include "incl.h"
 
 string replacement(vector<st> &rls, string &str, int i , int point) {
-    str[point] = rls[i].ns;
+    if(rls[i].ns != '*') {
+        str[point] = rls[i].ns;
+    }
+    else
+    {
+        str[point] = str[point];
+    }
+    
     return str;
 }
