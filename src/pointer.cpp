a79577e5188a7a020f07195a0824d97567f96a9e
Maxim <eremenko98@yandex.ru>
2020-08-16 13:59:18 +0700

    Change mark function

--- src/pointer.cpp
+++ src/pointer.cpp
@@ -1,7 +1,11 @@
 #include "incl.h"
 
-void pointer(int point) {
-  for (int i = 0; i < point; i++)
-    cout << " ";
-  cout << "^" << endl;
+void pointer(int point, string str) {
+   for(unsigned int i = 0; i < str.length(); i++) {
+       cout << str[i];
+       if(i == (unsigned)point-1) {
+           cout << "[" << str[point] << "]";
+       }
+   }
+   cout << endl;
 }
--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -2,15 +2,15 @@
 
 extern string replacement(vector<st> &rls, string &str, int i, int point);
 extern int move(int point, vector<st> &rls, int i);
-extern void pointer(int point);
+extern void pointer(int point, string str);
 
 void string_processing(string &str, vector<st> &rls) {
   int i = 0, point = 0;
   string that_position = rls[0].fp;
   while (that_position != "halt") {
     if (that_position == rls[i].fp && str[point] == rls[i].es) {
-      cout << replacement(rls, str, i, point) << endl;
-      pointer(point);
+      replacement(rls, str, i, point);
+      pointer(point, str);
       point = move(point, rls, i);
       that_position = rls[i].np;
       i = 0;
