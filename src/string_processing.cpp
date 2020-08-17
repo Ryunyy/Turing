d08c854ae327f2f64d461137c13dbdbaf44f01de
Maxim <eremenko98@yandex.ru>
2020-08-17 11:51:05 +0700

    Error output for non-existent rule

--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -8,6 +8,18 @@ void string_processing(string &str, vector<st> &rls) {
   int i = 0, point = 0;
   string that_position = rls[0].fp;
   while (that_position != "halt") {
+    int j = 0;
+    for (auto s : rls) {
+      if (s.es == str[point] && s.fp == that_position) {
+        j++;
+      }
+    }
+      if(j){
+        j = 0;
+      } else {
+        cout << "rules for '" << str[point] << "' is not exist in " << that_position << " statement" <<  endl;
+        break;
+      }
     if (that_position == rls[i].fp && str[point] == rls[i].es) {
       replacement(rls, str, i, point);
       pointer(point, str);
