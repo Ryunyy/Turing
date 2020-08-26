03704e9d202f76375f30e9caaa5355a13a1176df
Maxim <eremenko98@yandex.ru>
2020-08-26 17:35:36 +0700

    Checking current state by length

--- src/check_rules.cpp
+++ src/check_rules.cpp
@@ -4,4 +4,9 @@ bool check_rules(vector<st> &rule) {
     if(rule[0].fp != "q1") {
         return 0;
     }
+    for(st i : rule) {
+        if(i.fp.length() > 10) {
+            return 0;
+        }
+    }
 }
