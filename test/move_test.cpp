c7a6764c417836eb3c368808219c0c3e4a429830
Maxim <eremenko98@yandex.ru>
2020-08-28 13:13:51 +0700

    Add first test case

--- test/move_test.cpp
+++ test/move_test.cpp
@@ -1,11 +1,21 @@
 #include "incl_test.h"
-int move(int point, st rule) {
-    if(rule.mv == "r")
+int move(int point, vector<st> &rule, int i) {
+    if(rule[i].mv == "r")
         point++;
-    if(rule.mv == "l")
+    if(rule[i].mv == "l")
         point--;
-    if(rule.mv == "n")
+    if(rule[i].mv == "n")
         point = point;
     return point;
 }
 
+TEST_CASE() {
+    std::vector<st> rules;
+    rules.resize(1);
+    rules[0].fp = "q1";
+    rules[0].es = "1";
+    rules[0].ns = "0";
+    rules[0].mv = "r"
+    rules[0].np = "halt"
+    REQUIRE(move(0, rules, 0) == 1);
+}
