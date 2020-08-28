938627c35b6a4dc45f2d6f401eb1c7f6b4d43b2d
Maxim <eremenko98@yandex.ru>
2020-08-28 13:02:18 +0700

    Create header file for test

---
+++ test/incl_test.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <array>
+#include <clocale>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+struct st {
+  string fp;
+  string es;
+  string ns;
+  string mv;
+  string np;
+};
