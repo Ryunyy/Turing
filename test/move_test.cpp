4916577f5beda65ab31a793b4bc773e580aa255d
Maxim <eremenko98@yandex.ru>
2020-08-30 18:45:02 +0700

    Fix test functions

--- test/move_test.cpp
+++ test/move_test.cpp
@@ -1,10 +1,10 @@
 #include "incl_test.h"
-int move(int point, vector<st> &rule, int i) {
-    if(rule[i].mv == "r")
+int move(int point, st rule) {
+    if(rule.mv == "r")
         point++;
-    if(rule[i].mv == "l")
+    if(rule.mv == "l")
         point--;
-    if(rule[i].mv == "n")
+    if(rule.mv == "n")
         point = point;
     return point;
 }
@@ -17,35 +17,35 @@ TEST_CASE() {
     rules[0].ns = "0";
     rules[0].mv = "r";
     rules[0].np = "halt";
-    REQUIRE(move(0, rules, 0) == 1);
+    REQUIRE(move(0, rules[0]) == 1);
     rules[0].fp = "q1";
     rules[0].es = "1";
     rules[0].ns = "0";
     rules[0].mv = "n";
     rules[0].np = "halt";
-    REQUIRE(move(2, rules, 0) == 2);
+    REQUIRE(move(2, rules[0]) == 2);
     rules[0].fp = "q1";
     rules[0].es = "1";
     rules[0].ns = "0";
     rules[0].mv = "l";
     rules[0].np = "halt";
-    REQUIRE(move(5, rules, 0) == 4);
+    REQUIRE(move(5, rules[0]) == 4);
     rules[0].fp = "q1";
     rules[0].es = "1";
     rules[0].ns = "0";
     rules[0].mv = "r";
     rules[0].np = "halt";
-    REQUIRE(move(9999, rules, 0) == 10000);
+    REQUIRE(move(9999, rules[0]) == 10000);
     rules[0].fp = "q1";
     rules[0].es = "1";
     rules[0].ns = "0";
     rules[0].mv = "n";
     rules[0].np = "halt";
-    REQUIRE(move(4325, rules, 0) == 4325);
+    REQUIRE(move(4325, rules[0]) == 4325);
     rules[0].fp = "q1";
     rules[0].es = "1";
     rules[0].ns = "0";
     rules[0].mv = "l";
     rules[0].np = "halt";
-    REQUIRE(move(2222, rules, 0) == 2221);
+    REQUIRE(move(2222, rules[0]) == 2221);
 }
--- test/replacement_letters_test.cpp
+++ test/replacement_letters_test.cpp
@@ -1,5 +1,5 @@
 #include "incl_test.h"
-string replacement(st rule, string &str, int point) {
+string replacement(st rule, string str, int point) {
     if(rule.ns != "*") {
         str[point] = rule.ns[0];
     } else {
@@ -13,14 +13,14 @@ TEST_CASE() {
     vector<st> rules;
     rules.resize(1);
     rules[0].fp = "q1";
-    rules[0].es = "1"
-    rules[0].ns = "0"
-    rules[0].mv = "r"
-    rules[0].np = "halt"
-    REQUIRE(replacement(rules, "101", 0) == "001");
-    REQUIRE(replacement(rules, "101", 2) == "100");
+    rules[0].es = "1";
+    rules[0].ns = "0";
+    rules[0].mv = "r";
+    rules[0].np = "halt";
+    REQUIRE(replacement(rules[0], "101", 0) == "001");
+    REQUIRE(replacement(rules[0], "101", 2) == "100");
     rules[0].ns = "*";
-    REQUIRE(replacement(rules, "10101010101", 8) == "1010101010");
-    REQUIRE(replacement(rules, "q", 0) == "q");
-    
+    REQUIRE(replacement(rules[0], "10101010101", 8) == "10101010101");
+    REQUIRE(replacement(rules[0], "q", 0) == "q");
+
 }
